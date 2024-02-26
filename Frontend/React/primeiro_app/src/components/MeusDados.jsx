4100332abe7aeaa63f1af493d81a5ee59edf9eee
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2024-02-26 12:54:37 -0300

    Pimeiro_app_em_react && Auterando_bootstrap_com_SASS

---
+++ Frontend/React/primeiro_app/src/components/MeusDados.jsx
@@ -0,0 +1,14 @@
+export default function MeusDados(){
+    let nome="Guilherme Nunes"
+    let idade="19"
+    let profissao = "engenheiro de software"
+    
+    return(
+        <>
+        <h2>Meus Dados</h2>
+        <p>Nome: {nome}</p>
+        <p>Idade: {idade}</p>
+        <p>Profissão: {profissao}</p>
+        </>
+    )
+}
