4b3edcb2c935d5bd51a1238e3dcfa6b2d2df1543
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2024-02-27 01:37:21 -0300

    utilizando_css_no_react

--- Frontend/React/primeiro_app/src/components/MeusDados.jsx
+++ Frontend/React/primeiro_app/src/components/MeusDados.jsx
@@ -1,14 +1,23 @@
+import "./MeusDados.css"
+import logo from "./img/Logo-FIAP.png"
+
 export default function MeusDados(){
     let nome="Guilherme Nunes"
     let idade="19"
     let profissao = "engenheiro de software"
-    
+
+    let formParagr = {
+        color: "blue",
+        fontSize: "25px",
+        background: "yellow"
+    }
     return(
         <>
-        <h2>Meus Dados</h2>
-        <p>Nome: {nome}</p>
-        <p>Idade: {idade}</p>
-        <p>Profissão: {profissao}</p>
+        <h2>MEUS DADOS</h2>
+        <img src={logo} alt="Logo FIAP" width='100px'/>
+        <p style={{color:"red",textAlign:"center"}}>Nome: {nome}</p>
+        <p style={formParagr}>Idade: {idade}</p>
+        <p className="terceiro">Profissão: {profissao}</p>
         </>
     )
 }
