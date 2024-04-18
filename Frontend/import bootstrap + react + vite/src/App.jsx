6d3b0df88873431677bdd8fc5074678d3e40a546
Guilherme Nunes <123277481+Guisnu@users.noreply.github.com>
2024-04-18 13:17:23 -0300

    chore: Import Bootstrap + React + SASS + Vite

---
+++ Frontend/import bootstrap + react + vite/src/App.jsx
@@ -0,0 +1,14 @@
+// import * as bootstrap from "react-bootstrap";
+
+import { Button } from "react-bootstrap";
+
+export default function App() {
+
+  return (
+    <div>
+      <h1 className="display-1">teste</h1>
+      <Button>teste</Button>
+    </div>
+
+  )
+}
